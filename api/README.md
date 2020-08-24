e228748a32e11a8f88794c71daa89e375be3198c
Jakub Sw <nja.mt2@gmail.com>
2020-08-24 12:06:12 +0200

    Api added

---
+++ api/README.md
@@ -0,0 +1,14 @@
+## Availible routes 
+To read temperature
+/gettemp
+
+To read humidity
+/gethumid
+
+To turn on onboard led
+/setledon 
+
+To turn off onboard led
+/setledoff
+
+Token and device information are in config.js
---
+++ api/main.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const {device, token} = require('./config');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/gettemp', function(req, res){
+    axios.get(`https://mdash.net/api/v2/devices/${device}/rpc/Temp.Read?access_token=${token}`)
+    .then(function (response) {
+        res.send(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+
+app.get('/gethumid', function(req, res){
+    axios.get(`https://mdash.net/api/v2/devices/${device}/rpc/Humid.Read?access_token=${token}`)
+    .then(function (response) {
+        res.send(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+app.get('/setledon', function(req, res){
+    axios.get(`https://mdash.net/api/v2/devices/${device}/rpc/Led.On?access_token=${token}`)
+    .then(function (response) {
+        res.send(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+app.get('/setledoff', function(req, res){
+    axios.get(`https://mdash.net/api/v2/devices/${device}/rpc/Led.Off?access_token=${token}`)
+    .then(function (response) {
+        res.send(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+app.listen(4000);
